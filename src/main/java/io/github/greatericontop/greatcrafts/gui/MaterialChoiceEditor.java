c6391e204744ddd7021c5f77c22893605274e4c5
greatericontop <greateric@protonmail.com>
2024-01-10 20:57:35 -0500

    materialchoiceeditor (unfinished)

---
+++ src/main/java/io/github/greatericontop/greatcrafts/gui/MaterialChoiceEditor.java
@@ -0,0 +1,57 @@
+package io.github.greatericontop.greatcrafts.gui;
+
+import io.github.greatericontop.greatcrafts.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+
+public class MaterialChoiceEditor implements Listener {
+    private static final String INV_NAME = "§bEdit Material Choice Items";
+
+    private final GUIManager guiManager;
+    public MaterialChoiceEditor(GUIManager guiManager) {
+        this.guiManager = guiManager;
+    }
+
+    public void openNew(Player player) {
+        Inventory gui = Bukkit.createInventory(player, 54, INV_NAME);
+        gui.setItem(51, Util.createItemStack(Material.ENCHANTED_BOOK, 1, "§bEdit Material Choice Items",
+                "§fPlace items in any of the 51 other slots of this inventory.",
+                "§fFor this slot, §eany §fitem placed here (no exact match needed) can be used."));
+        gui.setItem(52, Util.createItemStack(Material.RED_STAINED_GLASS, 1, "§cDiscard Changes"));
+        gui.setItem(53, Util.createItemStack(Material.LIME_STAINED_GLASS, 1, "§aSave Changes"));
+
+        Map<String, Object> internalData = guiManager.guiData.get(player.getUniqueId());
+        int slotNumber = (int) internalData.get("currentSlot");
+        List<Material> materialChoices = ((List<List<Material>>) internalData.get("materialChoiceExtra")).get(slotNumber);
+        for (int i = 0; i < materialChoices.size(); i++) {
+            gui.setItem(i, new ItemStack(materialChoices.get(i), 1));
+        }
+
+        player.openInventory(gui);
+    }
+
+    @EventHandler()
+    public void onClick(InventoryClickEvent event) {
+        Inventory gui = event.getClickedInventory();
+        if (gui == null)  return;
+        if (!event.getView().getTitle().equals(INV_NAME))  return;
+        if (!event.getView().getTopInventory().equals(event.getClickedInventory()))  return; // must click top inventory
+        Player player = (Player) event.getWhoClicked();
+
+        Map<String, Object> internalData = guiManager.guiData.get(player.getUniqueId());
+        int slotNumber = (int) internalData.get("currentSlot");
+
+        // TODO: save, discard
+
+    }
+
+}
