b12c17b5f91150bad824e5c3821f12cc00269f4a
greatericonrop <greateric@protonmail.com>
2024-02-13 10:08:44 -0500

    pagination (i havent tested this yet tho)

--- src/main/java/io/github/greatericontop/greatcrafts/gui/RecipeListMenu.java
+++ src/main/java/io/github/greatericontop/greatcrafts/gui/RecipeListMenu.java
@@ -19,19 +19,26 @@ import java.util.List;
 
 public class RecipeListMenu implements Listener {
     private static final String INV_NAME = "§3Recipes";
+    private static final int PREV_PAGE_SLOT = 45;
+    private static final int PAGE_NUMBER_INDICATOR_SLOT = 49;
+    private static final int NEXT_PAGE_SLOT = 53;
+    private static final int CRAFTS_PER_PAGE = 36;
 
     private final NamespacedKey recipeKeyPDC;
+    private final NamespacedKey pageNumberIndicatorPDC;
     private final GreatCrafts plugin;
     public RecipeListMenu(GreatCrafts plugin) {
         this.plugin = plugin;
         this.recipeKeyPDC = new NamespacedKey(plugin, "recipeKey");
+        this.pageNumberIndicatorPDC = new NamespacedKey(plugin, "pageNumberIndicator");
     }
 
-    public void openNew(Player player) {
-        List<SavedRecipe> allRecipes = plugin.recipeManager.getAllRecipes();
-        Inventory gui = Bukkit.createInventory(null, 54, INV_NAME);
-        int indexStart = 0; // TODO: pagination
-        for (int i = 0; i <= 35; i++) {
+    private void updateInventory(List<SavedRecipe> allRecipes, Inventory gui, int visualPageNumber) {
+        int indexStart = CRAFTS_PER_PAGE * (visualPageNumber - 1);
+        for (int i = 0; i < 54; i++) {
+            gui.setItem(i, null);
+        }
+        for (int i = indexStart; i < indexStart+CRAFTS_PER_PAGE; i++) {
             if (i >= allRecipes.size())  break;
             SavedRecipe savedRecipe = allRecipes.get(i);
             ItemStack icon = savedRecipe.iconItem();
@@ -47,7 +54,29 @@ public class RecipeListMenu implements Listener {
             icon.setItemMeta(im);
             gui.setItem(i, icon);
         }
+        int totalPages = (int) Math.ceil(allRecipes.size() / (double) CRAFTS_PER_PAGE);
+        // Previous page
+        if (visualPageNumber != 0) {
+            ItemStack prevPage = Util.createItemStack(Material.ARROW, 1, "Previous Page");
+            gui.setItem(PREV_PAGE_SLOT, prevPage);
+        }
+        // Next page
+        if (visualPageNumber != totalPages) {
+            ItemStack nextPage = Util.createItemStack(Material.ARROW, 1, "Next Page");
+            gui.setItem(NEXT_PAGE_SLOT, nextPage);
+        }
+        // Page number indicator
+        ItemStack nextPage = Util.createItemStack(Material.PAPER, 1, String.format("Page %d / %d", visualPageNumber, totalPages));
+        ItemMeta im = nextPage.getItemMeta();
+        im.getPersistentDataContainer().set(pageNumberIndicatorPDC, PersistentDataType.INTEGER, visualPageNumber);
+        nextPage.setItemMeta(im);
+        gui.setItem(PAGE_NUMBER_INDICATOR_SLOT, nextPage);
+    }
 
+    public void openNew(Player player) {
+        List<SavedRecipe> allRecipes = plugin.recipeManager.getAllRecipes();
+        Inventory gui = Bukkit.createInventory(null, 54, INV_NAME);
+        updateInventory(allRecipes, gui, 1);
         // (Does not get added to :playerMainInventories:)
         player.openInventory(gui);
     }
@@ -55,21 +84,29 @@ public class RecipeListMenu implements Listener {
     @EventHandler()
     public void onClick(InventoryClickEvent event) {
         Inventory gui = event.getClickedInventory();
-        if (gui == null)  return;
-        if (!event.getView().getTitle().equals(INV_NAME))  return;
-        if (!event.getView().getTopInventory().equals(event.getClickedInventory()))  return; // must click top inventory
+        if (gui == null) return;
+        if (!event.getView().getTitle().equals(INV_NAME)) return;
+        event.setCancelled(true); // cancel all when inventory open (including, e.g., shift-clicking bottom inventory)
+        if (!event.getView().getTopInventory().equals(event.getClickedInventory())) return; // must click top inventory
         Player player = (Player) event.getWhoClicked();
-
         int slot = event.getSlot();
-        // TODO: special slots for pagination
 
-        ItemStack itemClicked = event.getCurrentItem();
-        if (itemClicked == null || itemClicked.getType() == Material.AIR)  return;
-        ItemMeta im = itemClicked.getItemMeta();
-        String recipeKey = im.getPersistentDataContainer().get(recipeKeyPDC, PersistentDataType.STRING);
-        event.setCancelled(true);
-        player.closeInventory();
-        player.chat(String.format("/editrecipe %s", recipeKey));
+        if (slot == PREV_PAGE_SLOT) {
+            int pageNum = gui.getItem(PAGE_NUMBER_INDICATOR_SLOT).getItemMeta().getPersistentDataContainer().get(pageNumberIndicatorPDC, PersistentDataType.INTEGER);
+            List<SavedRecipe> allRecipes = plugin.recipeManager.getAllRecipes();
+            updateInventory(allRecipes, gui, pageNum-1);
+        } else if (slot == NEXT_PAGE_SLOT) {
+            int pageNum = gui.getItem(PAGE_NUMBER_INDICATOR_SLOT).getItemMeta().getPersistentDataContainer().get(pageNumberIndicatorPDC, PersistentDataType.INTEGER);
+            List<SavedRecipe> allRecipes = plugin.recipeManager.getAllRecipes();
+            updateInventory(allRecipes, gui, pageNum+1);
+        } else if (slot < CRAFTS_PER_PAGE) {
+            ItemStack itemClicked = event.getCurrentItem();
+            if (itemClicked == null || itemClicked.getType() == Material.AIR) return;
+            ItemMeta im = itemClicked.getItemMeta();
+            String recipeKey = im.getPersistentDataContainer().get(recipeKeyPDC, PersistentDataType.STRING);
+            player.closeInventory();
+            player.chat(String.format("/editrecipe %s", recipeKey));
+        }
     }
 
 }
