45522e06bddb5811559e81bd879ee81e04e3aa5c
greatericontop <greateric@protonmail.com>
2024-05-19 20:45:53 -0400

    apparently its actually 2023

--- src/main/java/io/github/greatericontop/greatcrafts/internal/datastructures/RecipeType.java
+++ src/main/java/io/github/greatericontop/greatcrafts/internal/datastructures/RecipeType.java
@@ -1,7 +1,7 @@
 package io.github.greatericontop.greatcrafts.internal.datastructures;
 
 /*
- * Copyright (C) 2024-present greateric.
+ * Copyright (C) 2023-present greateric.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
