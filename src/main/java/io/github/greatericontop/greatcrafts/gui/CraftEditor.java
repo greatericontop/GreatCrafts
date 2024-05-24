d3e7b7964ad5fd392d2507c97a8e74d2ac3a8ec7
greatericontop <greateric@protonmail.com>
2024-05-24 10:52:24 -0400

    dont break when the recipe editing doesnt exist

--- src/main/java/io/github/greatericontop/greatcrafts/gui/CraftEditor.java
+++ src/main/java/io/github/greatericontop/greatcrafts/gui/CraftEditor.java
@@ -73,6 +73,10 @@ public class CraftEditor implements Listener {
 
     public void openNew(Player player, String craftKey) {
         SavedRecipe savedRecipe = guiManager.getRecipeManager().getRecipe(craftKey);
+        if (savedRecipe == null) {
+            player.sendMessage("§cRecipe not found!");
+            return;
+        }
         Inventory gui = Bukkit.createInventory(player, 54, INV_NAME);
 
         for (int i = 0; i < 54; i++) {
