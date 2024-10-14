b993d0c82c337e3bf2c999328efd70334aa0bcc9
greatericontop <greateric@protonmail.com>
2024-10-13 21:37:46 -0400

    add shapeless crafters

--- src/main/java/io/github/greatericontop/greatcrafts/events/stackeditems/CrafterEvents.java
+++ src/main/java/io/github/greatericontop/greatcrafts/events/stackeditems/CrafterEvents.java
@@ -35,6 +35,9 @@ import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CrafterEvents implements Listener {
 
     private final GreatCrafts plugin;
@@ -51,22 +54,18 @@ public class CrafterEvents implements Listener {
             if (savedRecipe == null || savedRecipe.type() != RecipeType.STACKED_ITEMS) {
                 return;
             }
-            processCrafterShapelessStackedItems(event, savedRecipe, recipeKey);
+            processCrafterShapedStackedItems(event, savedRecipe, recipeKey);
         } else if (_rawRecipe instanceof ShapelessRecipe _shapelessRec) {
             NamespacedKey recipeKey = _shapelessRec.getKey();
             SavedRecipe savedRecipe = plugin.recipeManager.getRecipe(recipeKey.toString());
             if (savedRecipe == null || savedRecipe.type() != RecipeType.STACKED_ITEMS_SHAPELESS) {
                 return;
             }
-            // TODO: WIP
-            event.setCancelled(true);
-            for (Entity e : event.getBlock().getWorld().getNearbyEntities(event.getBlock().getLocation(), 10.0, 10.0, 10.0, entity -> entity instanceof Player)) {
-                e.sendMessage("§cCrafters cannot craft stacked items recipes yet!");
-            }
+            processCrafterShapelessStackedItems(event, savedRecipe, recipeKey);
         }
     }
 
-    private void processCrafterShapelessStackedItems(CrafterCraftEvent event, SavedRecipe savedRecipe, NamespacedKey recipeKey) {
+    private void processCrafterShapedStackedItems(CrafterCraftEvent event, SavedRecipe savedRecipe, NamespacedKey recipeKey) {
         // Crafters always craft one item at a time
         // Simple adaptation of the code in StackedItemsCraftListener, follows more or less the same logic
         event.setCancelled(true);
@@ -113,6 +112,55 @@ public class CrafterEvents implements Listener {
             ItemStack stack = eventInv.getItem(slotNum+savedRecToEventInvOffset);
             stack.setAmount(stack.getAmount() - required + 1);
             eventInv.setItem(slotNum+savedRecToEventInvOffset, stack);
+        }
+        event.setCancelled(false);
+    }
+
+    private void processCrafterShapelessStackedItems(CrafterCraftEvent event, SavedRecipe savedRecipe, NamespacedKey recipeKey) {
+        // Similar to other one, again
+        Crafter blockState = (Crafter) event.getBlock().getState();
+        Inventory eventInv = blockState.getInventory();
+        // Algo: Iterate through all required items (starting from the highest quantity required), and match it to the
+        //       highest quantity of corresponding material in the slot. Use this to find out the maximum crafts we
+        //       can get, and then remove both from the list.
+        event.setCancelled(true);
+        List<ItemStack> requiredItems = new ArrayList<>(savedRecipe.items()); // clone
+        List<ItemStack> inventorySlotItems = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) { // 0 through 8
+            inventorySlotItems.add(eventInv.getItem(i));
+        }
+        int[] requiredItemsSlotMapping = new int[9]; // RISM[inventory slot 0-8] = index to use in requiredItems
+        requiredItems.removeIf(item -> (item == null || item.getType() == Material.AIR)); // remove empty
+        requiredItems.sort((a, b) -> Integer.compare(b.getAmount(), a.getAmount()));
+        // Matching step (greedy-style pairing the highest quantity requirement to the highest quantity of item)
+        for (int reqIndex = 0; reqIndex < requiredItems.size(); reqIndex++) {
+            ItemStack reqItemStack = requiredItems.get(reqIndex);
+            int highestCountSoFar = 0;
+            int highestCountSlot = -1;
+            for (int slotNum = 0; slotNum < 9; slotNum++) {
+                ItemStack inventorySlotItem = inventorySlotItems.get(slotNum);
+                if (inventorySlotItem == null) continue;
+                if (inventorySlotItem.getType() != reqItemStack.getType()) continue;
+                if (highestCountSoFar < inventorySlotItem.getAmount()) {
+                    highestCountSoFar = inventorySlotItem.getAmount();
+                    highestCountSlot = slotNum;
+                }
+            }
+            if (highestCountSoFar < reqItemStack.getAmount()) {
+                // Fail silently again
+                return;
+            }
+            inventorySlotItems.set(highestCountSlot, null); // we can't use this again
+            requiredItemsSlotMapping[highestCountSlot] = reqIndex; // Link up this inventory slot to the required ingredient so we know how many to remove later
+        }
+
+        // Remove items (again leaving 1) and uncancel
+        for (int slotIndex = 0; slotIndex < 9; slotIndex++) {
+            ItemStack stack = eventInv.getItem(slotIndex); // these match now (no off by 1 like in the other case)
+            if (stack == null || stack.getType() == Material.AIR)  continue;
+            int amountToRemove = requiredItems.get(requiredItemsSlotMapping[slotIndex]).getAmount() - 1;
+            stack.setAmount(stack.getAmount() - amountToRemove);
+            eventInv.setItem(slotIndex, stack);
         }
         event.setCancelled(false);
     }
