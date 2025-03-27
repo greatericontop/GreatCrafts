77ff9ca5e20c96dd773da4c8b6807fc2dbef617b
greatericontop <greateric@protonmail.com>
2025-03-27 12:13:53 -0400

    player check only applies to editing recipes

--- src/main/java/io/github/greatericontop/greatcrafts/commands/EditRecipeCommand.java
+++ src/main/java/io/github/greatericontop/greatcrafts/commands/EditRecipeCommand.java
@@ -34,37 +34,38 @@ public class EditRecipeCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) { // TODO: only for editing
-            plugin.languager.commandErrorPlayerRequired(sender);
-            return true;
-        }
-        if (player.getGameMode() != GameMode.CREATIVE) {
-            plugin.languager.commandErrorCreativeRequired(player);
-            return true;
-        }
         if (args.length == 0) {
             return false;
         }
         String recipeName = args[0];
         SavedRecipe savedRecipe = plugin.recipeManager.getRecipe(recipeName);
         if (savedRecipe == null) {
-            plugin.languager.commandErrorRecipeNotExist(player, recipeName);
+            plugin.languager.commandErrorRecipeNotExist(sender, recipeName);
             return true;
         }
+
         if (args.length == 1) {
+            if (!(sender instanceof Player player)) {
+                plugin.languager.commandErrorPlayerRequired(sender);
+                return true;
+            }
+            if (player.getGameMode() != GameMode.CREATIVE) {
+                plugin.languager.commandErrorCreativeRequired(player);
+                return true;
+            }
             plugin.guiCraftEditor.openNew(player, recipeName);
             return true;
         }
 
         String setting = GreatCommands.argumentStringFromChoices(1, args, new String[]{"auto-unlock-setting"});
         if (setting == null) {
-            player.sendMessage("The setting must be one of the choices"); // TODO: language
+            sender.sendMessage("The setting must be one of the choices"); // TODO: language
             return true;
         }
         if (setting.equals("auto-unlock-setting")) {
             String value = GreatCommands.argumentStringFromChoices(2, args, new String[]{"never", "have-each", "have-one", "always"});
             if (value == null) {
-                player.sendMessage("The value for auto-unlock-setting must be one of the choices ..."); // TODO: language
+                sender.sendMessage("The value for auto-unlock-setting must be one of the choices ..."); // TODO: language
                 return true;
             }
             plugin.getConfig().set(String.format("automatically-unlock-recipes-exceptions.%s", recipeName), value);
